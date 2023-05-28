3454f0e10c18cc988d240cfa5de77c0e8a16d1d0
nameishappy <105534032+nameishappy@users.noreply.github.com>
2023-05-28 22:54:08 +0530

    push deploy command

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/bank_modern_app/"
+  base:"/bank_modern_app"
 })
