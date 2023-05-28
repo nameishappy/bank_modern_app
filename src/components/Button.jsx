5bc6bbfe6e56470a7ea2407ccacadda16a48ba23
nameishappy <105534032+nameishappy@users.noreply.github.com>
2023-05-28 21:58:11 +0530

    complete bank app

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Button = ({styles}) =>(
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins rounded-[12px] text-[18px] font-medium outline-none text-primary ${styles}`}>Get Started</button>
+  )
+
+
+export default Button
